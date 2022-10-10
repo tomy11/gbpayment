93007efe12c28dc9697dbdb63eda7c23332303d2
tomy11 <ohnemotive74@gmail.com>
2022-10-10 10:04:29 +0700

    create new app
    create app

---
+++ src/main/java/com/payment/gbpayment/GbpaymentApplication.java
@@ -0,0 +1,13 @@
+package com.payment.gbpayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GbpaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GbpaymentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/payment/gbpayment/GbpaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payment.gbpayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GbpaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
